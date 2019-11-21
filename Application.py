1a30a1906a1ddd8f2f8c7e9b2cb058bf5e74c4d2
Samrat Sahoo <samratsahoo2013@gmail.com>
2019-11-21 07:18:50 -0600

    Ported most to facial recognition class

--- Application.py
+++ Application.py
@@ -1,46 +1,111 @@
+import os
 import cv2
+import face_recognition
+import numpy as np
+import argparse
+import keyboard
+
 
 # Load Cascade Classifier
-import numpy as np
-
 faceCascade = cv2.CascadeClassifier('Cascades/data/haarcascade_frontalface_alt.xml')
 
 # Create Webcam
 video = cv2.VideoCapture(0)
 
-while (True):
-    # Open Webcam
+# Encoding Image for Samrat Sahoo
+samratImage = face_recognition.load_image_file("samrat.jpg")
+samratEncoding = face_recognition.face_encodings(samratImage)[0]
+
+# Encoding Image for Caitlin Fukumoto
+caitlinImage = face_recognition.load_image_file("caitlin.jpg")
+caitlinEncoding = face_recognition.face_encodings(caitlinImage)[0]
+
+# List For Face Encodings
+faceEncodingsKnown = [
+    samratEncoding,
+    caitlinEncoding
+]
+
+# List For Face Names
+faceNamesKnown = [
+    "Samrat",
+    "Caitlin"
+]
+
+faceLocations = []
+faceEncodings = []
+faceNames = []
+processThisFrame = True
+samratCounter = 0
+caitlinCounter = 0
+
+while True:
+    # Open Webcam + Optimize Webcam
     ret, frame = video.read()
+    smallFrame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-    # Analyze Gray Scale and increase brightness
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = np.uint8(np.double(gray) + 5)
+    # Change Webcam to RGB
+    rgbFrame = smallFrame[:, :, ::-1]
 
-    # Use Haar Cascade to find face
-    faces = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+    # Find face locations and then do facial recognition to it
+    if processThisFrame:
+        faceLocations = face_recognition.face_locations(rgbFrame)
+        faceEncodings = face_recognition.face_encodings(rgbFrame, faceLocations)
 
-    # Save Face as PNG
-    for(x,y,w,h) in faces:
-        roiGray = gray[y:y+h, x:x+w]
-        roiColor = frame[y:y+h, x:x+w]
-        imgItem = "myImage.png"
-        cv2.imwrite(imgItem, roiGray)
+        # make faceNames List empty every frame; refreshes to see if person still there
+        faceNames = []
 
-        # Conditions for Rectangle
-        color = (255,0,0)
-        stroke = 2
-        endX = x + w
-        endY = y + h
+        # Cycle through face encodings to find a match
+        for faceEncoding in faceEncodings:
+            matchFound = face_recognition.compare_faces(faceEncodingsKnown, faceEncoding)
+            name = "Not Found"
 
-        # Draw Rectangle
-        cv2.rectangle(frame, (x, y), (endX, endY), color, stroke)
+            faceDistances = face_recognition.face_distance(faceEncodingsKnown, faceEncoding)
+            matchIndex = np.argmin(faceDistances)
+            if matchFound[matchIndex]:
+                name = faceNamesKnown[matchIndex]
+
+            # Add names to faceNames list once found
+            faceNames.append(name)
+
+    processThisFrame = not processThisFrame
+
+    for (top, right, bottom, left), name in zip(faceLocations, faceNames):
+        # scaling again to correct for previous scaling
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        # Draw a Rectangle
+        cv2.rectangle(frame, (left, top), (right, bottom), (255, 0, 0), 2)
+
+        # Draw a label with a name
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (255, 0, 0), cv2.FILLED)
+
+        # Set label font + draw Text
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        # Display name in console Once only
+        if samratCounter == 0 and name is 'Samrat':
+            print(name)
+            samratCounter = samratCounter + 1
+
+        if caitlinCounter == 0 and name is 'Caitlin':
+            print(name)
+            caitlinCounter = caitlinCounter + 1
+
 
     # Show Frame
     cv2.imshow('frame', frame)
 
+    # If q is pressed, exit loop
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
 # Upon exiting while loop, close web cam
 video.release()
-video.destroyAllWindows()
+
+
+
